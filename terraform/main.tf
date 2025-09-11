37546ea0c65ab308c201a95f652f41d3053c07d1
littl3-kitty <153267672+littl3-kitty@users.noreply.github.com>
2025-09-11 21:01:44 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "aws_security_group" "web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ip]
   }
 
   egress {
